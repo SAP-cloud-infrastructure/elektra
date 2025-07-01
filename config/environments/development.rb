b34f1109424ba9199cbf3802505c14592486b562
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-07-01 11:15:30 +0200

    chore(core): adds a comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -96,4 +96,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  
+  # Uncomment this line when testing email service
+  # When generating URLs (like admin_inquiries_url) from a background job or a mailer, you need to tell Rails what host to use from rails c.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
