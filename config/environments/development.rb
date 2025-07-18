4268a2e9371e2c22db701ebf0df917e96f12f4c6
Andreas Pfau <andreas.pfau@sap.com>
2025-07-18 12:43:27 +0200

    chore(core): update ruby and rails versions (#1651)
    Co-authored-by: hgw77 <hans-georg.winkler@sap.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,12 +3,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-
-  # if the dev enviornemnt not running localy this config is needed e.g. workspaces 
+  config.enable_reloading = true
+    # if the dev enviornemnt not running localy this config is needed e.g. workspaces 
   config.hosts << /.*\.cloud\.sap/
 
   # Do not eager load code on boot.
@@ -59,13 +59,24 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
-  # web_console is only working when you accessing from localhost
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
+# web_console is only working when you accessing from localhost
   # if you running the development server on a remote machine use TRUSTED_IP
   # for that take a look to the .env.bak and set the variable or run "TRUSTED_IP=192.168.1.1 forman start"
   if ENV['TRUSTED_IP']
@@ -101,4 +112,5 @@ Rails.application.configure do
   # Uncomment this line when testing email service
   # When generating URLs (like admin_inquiries_url) from a background job or a mailer, you need to tell Rails what host to use from rails c.
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
