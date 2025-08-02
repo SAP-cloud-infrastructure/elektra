d5606295cc4f18f7dd983c7f743407bfc9182b71
VoigtS <s.voigt@sap.com>
2025-08-02 20:45:56 +0200

    feat(plugins): transform remaining static 'resource' keys to constants

--- plugins/shared_filesystem_storage/app/javascript/widgets/app/containers/castellum/configuration/view.js
+++ plugins/shared_filesystem_storage/app/javascript/widgets/app/containers/castellum/configuration/view.js
@@ -2,14 +2,16 @@ import { connect } from "react-redux"
 import CastellumConfigurationView from "../../../components/castellum/configuration/view"
 import { disableAutoscaling } from "../../../actions/castellum"
 import { fetchShareTypesIfNeeded } from "../../../actions/share_types"
+import { CASTELLUM_AUTOSCALING } from "../../../constants"
 
 export default connect(
   (state) => ({
-    config: (state.castellum || {})["resources"],
+    config: (state.castellum || {})[CASTELLUM_AUTOSCALING.key],
     shareTypes: state.shareTypes,
   }),
   (dispatch) => ({
-    disableAutoscaling: (projectID, shareTypes, allShares) => dispatch(disableAutoscaling(projectID, shareTypes, allShares)),
+    disableAutoscaling: (projectID, shareTypes, allShares) =>
+      dispatch(disableAutoscaling(projectID, shareTypes, allShares)),
     loadShareTypesOnce: () => dispatch(fetchShareTypesIfNeeded()),
   })
 )(CastellumConfigurationView)
--- plugins/shared_filesystem_storage/app/javascript/widgets/app/containers/castellum/tabs.js
+++ plugins/shared_filesystem_storage/app/javascript/widgets/app/containers/castellum/tabs.js
@@ -1,13 +1,14 @@
 import { connect } from "react-redux"
 import CastellumTabs from "../../components/castellum/tabs"
 import { fetchCastellumDataIfNeeded } from "../../actions/castellum"
+import { CASTELLUM_AUTOSCALING } from "../../constants"
 
 export default connect(
   (state) => ({
-    config: (state.castellum || {})["resources"],
+    config: (state.castellum || {})[CASTELLUM_AUTOSCALING.key],
   }),
   (dispatch) => ({
     loadResourceConfigOnce: (projectID) =>
-      dispatch(fetchCastellumDataIfNeeded(projectID, null, "resources")),
+      dispatch(fetchCastellumDataIfNeeded(projectID, null, CASTELLUM_AUTOSCALING.key)),
   })
 )(CastellumTabs)
