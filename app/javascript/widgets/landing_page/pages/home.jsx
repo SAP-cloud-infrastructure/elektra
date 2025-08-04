76e40fab3206dc10b9f18324131db81da6299aad
hgw77 <hans-georg.winkler@sap.com>
2025-08-04 16:13:12 +0200

    chore(core): adjust spacing

--- app/javascript/widgets/landing_page/pages/home.jsx
+++ app/javascript/widgets/landing_page/pages/home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
   return (
     <div className="tw-flex tw-flex-col tw-grow">
       <LoginOverlay />
-      <div className="tw-max-w-[1280px] tw-w-full tw-mx-auto tw-pt-8">
+      <div className="tw-max-w-[1280px] tw-w-full tw-mx-auto tw-pt-12">
         <Stack alignment="center">
           <div className="tw-text-xl tw-w-3/5 tw-mr-auto">
             {"SAP's "} strategic Infrastructure-as-a-Service (IaaS) stack, optimized for SAP solutions, running purely
