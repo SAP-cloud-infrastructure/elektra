eeb0121c26dcbdf22a804927b685f3a72e4dc732
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2025-08-27 14:35:01 +0200

    chore(ui): move jsoneditor svg to global assets, switch lookup plugin to use ui-components json viewer (#1697)

--- plugins/lookup/app/javascript/widgets/reverse_app/components/jsonEditor.jsx
+++ plugins/lookup/app/javascript/widgets/reverse_app/components/jsonEditor.jsx
@@ -1,22 +1,19 @@
 import init_json_editor from "lib/jsoneditor"
 import React from "react"
+import { AppShellProvider, CodeBlock } from "@cloudoperators/juno-ui-components"
 
 class JsonEditor extends React.Component {
-  componentDidMount() {
-    // eslint-disable-next-line no-undef
-    init_json_editor()
-  }
-
   render() {
     return (
-      <div className="objectinfo_json_editor">
-        <b>{this.props.title}</b>
-        <div
-          id="jsoneditor"
-          data-mode="view"
-          data-content={JSON.stringify(this.props.details, null, 2)}
+      <AppShellProvider theme="theme-light">
+        <CodeBlock
+          heading="Metadata"
+          content={this?.props?.details || null}
+          lang="json"
+          className="mt-6"
+          style={{ marginTop: "1.5rem" }}
         />
-      </div>
+      </AppShellProvider>
     )
   }
 }
