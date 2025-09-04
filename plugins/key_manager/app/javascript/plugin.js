193c781d1c69ff6c8f445175052bf5a1030c5970
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2025-09-04 14:04:33 +0200

    chore(core): decommission old Key Manager plugin (#1718)

--- plugins/key_manager/app/javascript/plugin.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import "./key_manager"
-import "./secrets_table"
-
-import "lib/jsoneditor"
-
-// This is always executed on page load.
-$(document).ready(init_json_editor)
