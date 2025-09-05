e6f4740bab0dc9738add9140203516b6d91b2bbb
hgw77 <hans-georg.winkler@sap.com>
2025-09-05 09:40:44 +0200

    chore(core): upgrade to rails 7.1.5.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,21 +3,16 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
-    # if the dev enviornemnt not running localy this config is needed e.g. workspaces 
-  config.hosts << /.*\.cloud\.sap/
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  # Do not render the standard error page in development.
-  config.action_dispatch.show_exceptions = false
 
   # Enable server timing
   config.server_timing = true
@@ -78,38 +73,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-
-  # Allow web console access from any IP this is ok in development 🙃
-  config.web_console.whitelisted_ips = '0.0.0.0/0'
-
-  # Mailer configuration for inquiries/requests
-  config.action_mailer.perform_deliveries = false
-
-  puts "=> Region: #{ENV['MONSOON_DASHBOARD_REGION']}" if ENV['MONSOON_DASHBOARD_REGION']
-  puts "=> Auth Endpoint #{ENV['MONSOON_OPENSTACK_AUTH_API_ENDPOINT']}" if ENV['MONSOON_OPENSTACK_AUTH_API_ENDPOINT']
-
-  # reduce active record logging
-  config.after_initialize do
-    if ENV['ACTIVE_RECORD_QUIET']
-      ActiveRecord::Base.logger = Rails.logger.clone
-      ActiveRecord::Base.logger.level = Logger::INFO
-      puts "=> ActiveRecord Logging: QUIET"
-    end
-  end
-
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  puts "=> Elektron Logging: QUIET" if ENV['ELEKTRON_QUIET']
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-  
-  # Uncomment this line when testing email service
-  # When generating URLs (like admin_inquiries_url) from a background job or a mailer, you need to tell Rails what host to use from rails c.
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  # Enable stdout logger
-  config.logger = Logger.new(STDOUT)
-
 end
