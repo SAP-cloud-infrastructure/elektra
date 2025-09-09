2b822e6d34ce6ed0bf6eab1b5094d46882079435
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2025-09-09 14:26:13 +0200

    chore(kubernetes): ts and tanstack router setup for kubernetes_ng (#1724)

--- plugins/kubernetes_ng/app/javascript/widgets/app/apiClient.ts
+++ plugins/kubernetes_ng/app/javascript/widgets/app/apiClient.ts
@@ -2,4 +2,6 @@ import { createAjaxHelper } from "lib/ajax_helper"
 import { widgetBasePath } from "lib/widget"
 
 const baseURL = widgetBasePath("kubernetes_ng")
-export default createAjaxHelper({ baseURL })
+export const apiClient = createAjaxHelper({ baseURL })
+
+export type ApiClient = ReturnType<typeof createAjaxHelper>
