e54744178ceb848ce334d6069074afd9d8ee2199
andypf <andreas.pfau@sap.com>
2025-09-18 16:40:00 +0200

    chore(core): remove debug logs

--- lib/generators/plugin_skeleton_generator.rb
+++ lib/generators/plugin_skeleton_generator.rb
@@ -19,7 +19,6 @@ class PluginSkeletonGenerator
   end
 
   def run
-    pp options.public_methods.sort
     plugin_options =
       "--skip-gemfile --skip-bundle --skip-git --skip-test --skip-puma --skip-javascript --skip-gemfile-entry"
     plugin_options += " --mountable" if options.mountable?
