96b238cfd5f68b2fe28490f9d6b9b4830734d11b
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-09-23 08:54:06 +0200

    chore(kubernetes): adds loading to clusters detail view

--- plugins/kubernetes_ng/app/javascript/widgets/app/routes/clusters/$clusterName.tsx
+++ plugins/kubernetes_ng/app/javascript/widgets/app/routes/clusters/$clusterName.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, Await } from "@tanstack/react-router"
 import { Cluster } from "../../types/clusters"
 import { LoaderWithCrumb } from "../-types"
 import {
@@ -13,6 +13,7 @@ import {
   DataGridHeadCell,
   DataGridCell,
   Stack,
+  Spinner,
 } from "@cloudoperators/juno-ui-components"
 import PageHeader from "../../components/PageHeader"
 import CustomPopupMenu from "../../components/PopupMenu"
@@ -21,9 +22,9 @@ import RedinessConditions from "../../components/RedinessConditions"
 
 export const Route = createFileRoute("/clusters/$clusterName")({
   component: ClusterDetail,
-  loader: async ({ context, params }): Promise<LoaderWithCrumb & { cluster: Cluster }> => {
+  loader: async ({ context, params }): Promise<LoaderWithCrumb & { clusterDetailsPromise: Promise<Cluster> }> => {
     const client = context.apiClient
-    const details = await client
+    const clusterDetailsPromise = client
       ?.get<{ data: Cluster }>(`/kubernetes-ng/api/clusters/${params.clusterName}/`)
       .then((response) => response.data)
 
@@ -33,7 +34,7 @@ export const Route = createFileRoute("/clusters/$clusterName")({
       crumb: {
         label: `${params.clusterName}`,
       },
-      cluster: details || ({} as Cluster),
+      clusterDetailsPromise,
     }
   },
 })
@@ -41,133 +42,146 @@ export const Route = createFileRoute("/clusters/$clusterName")({
 const sectionHeaderStyles = "section tw-text-lg tw-font-bold tw-mb-4"
 
 function ClusterDetail() {
-  const { cluster } = Route.useLoaderData()
+  const { clusterDetailsPromise } = Route.useLoaderData()
   const [displayJson, setDisplayJson] = useState(false)
   return (
-    <>
-      <PageHeader title={`Cluster ${cluster.name} Information`}>
-        <Button size="small" label="Delete Cluster" variant="primary-danger" />
-        <Button size="small" label="Edit Cluster" />
-        <CustomPopupMenu>
-          <div onClick={() => setDisplayJson(true)}>
-            <PopupMenuItem label="JSON" />
-          </div>
-        </CustomPopupMenu>
-      </PageHeader>
+    <Await
+      promise={clusterDetailsPromise}
+      fallback={
+        <Container py px={false}>
+          <Spinner size="small" />
+        </Container>
+      }
+    >
+      {(cluster) => {
+        return (
+          <>
+            <PageHeader title={`Cluster ${cluster.name} Information`}>
+              <Button size="small" label="Delete Cluster" variant="primary-danger" />
+              <Button size="small" label="Edit Cluster" />
+              <CustomPopupMenu>
+                <div onClick={() => setDisplayJson(true)}>
+                  <PopupMenuItem label="JSON" />
+                </div>
+              </CustomPopupMenu>
+            </PageHeader>
 
-      {/* Basic info */}
-      <Container py px={false}>
-        <p className={sectionHeaderStyles}>Basic information</p>
-        <DataGrid columns={2} gridColumnTemplate="50% 50%">
-          <DataGridRow>
-            <div>
-              <DataGrid columns={2} gridColumnTemplate="40% auto">
+            {/* Basic info */}
+            <Container py px={false}>
+              <p className={sectionHeaderStyles}>Basic information</p>
+              <DataGrid columns={2} gridColumnTemplate="50% 50%">
                 <DataGridRow>
-                  <DataGridHeadCell>Name</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      {cluster.name}
-                    </Stack>
-                  </DataGridCell>
-                </DataGridRow>
-                <DataGridRow>
-                  <DataGridHeadCell>ID</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      <ClipboardText text={cluster.uid} className="tw-ml-2" />
-                    </Stack>
-                  </DataGridCell>
-                </DataGridRow>
-                <DataGridRow>
-                  <DataGridHeadCell>Cluster Status</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      {cluster.status}
-                    </Stack>
-                  </DataGridCell>
-                </DataGridRow>
-                <DataGridRow>
-                  <DataGridHeadCell>Kubernetes Version</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      {cluster.version}
-                    </Stack>
-                  </DataGridCell>
-                </DataGridRow>
-                <DataGridRow>
-                  <DataGridHeadCell>Cloud Profile</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      {cluster.cloudProfileName}
-                    </Stack>
-                  </DataGridCell>
+                  <div>
+                    <DataGrid columns={2} gridColumnTemplate="40% auto">
+                      <DataGridRow>
+                        <DataGridHeadCell>Name</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            {cluster.name}
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                      <DataGridRow>
+                        <DataGridHeadCell>ID</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            <ClipboardText text={cluster.uid} className="tw-ml-2" />
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                      <DataGridRow>
+                        <DataGridHeadCell>Cluster Status</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            {cluster.status}
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                      <DataGridRow>
+                        <DataGridHeadCell>Kubernetes Version</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            {cluster.version}
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                      <DataGridRow>
+                        <DataGridHeadCell>Cloud Profile</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            {cluster.cloudProfileName}
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                    </DataGrid>
+                  </div>
+                  <div>
+                    <DataGrid columns={2}>
+                      <DataGridRow>
+                        <DataGridHeadCell>Purpose</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            {cluster.purpose}
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                      <DataGridRow>
+                        <DataGridHeadCell>Infrastructure</DataGridHeadCell>
+                        <DataGridCell>
+                          <Stack gap="1" direction="horizontal" wrap>
+                            {cluster.infrastructure}
+                          </Stack>
+                        </DataGridCell>
+                      </DataGridRow>
+                    </DataGrid>
+                  </div>
                 </DataGridRow>
               </DataGrid>
-            </div>
-            <div>
-              <DataGrid columns={2}>
-                <DataGridRow>
-                  <DataGridHeadCell>Purpose</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      {cluster.purpose}
-                    </Stack>
-                  </DataGridCell>
-                </DataGridRow>
-                <DataGridRow>
-                  <DataGridHeadCell>Infrastructure</DataGridHeadCell>
-                  <DataGridCell>
-                    <Stack gap="1" direction="horizontal" wrap>
-                      {cluster.infrastructure}
-                    </Stack>
-                  </DataGridCell>
-                </DataGridRow>
-              </DataGrid>
-            </div>
-          </DataGridRow>
-        </DataGrid>
-      </Container>
+            </Container>
 
-      <Container py px={false}>
-        <p className={sectionHeaderStyles}>Labels</p>
-        {cluster.labels && Object.keys(cluster.labels).length > 0 ? (
-          <DataGrid columns={2} minContentColumns={[0]}>
-            {Object.entries(cluster.labels).map(([key, value]) => (
-              <DataGridRow key={key}>
-                <DataGridHeadCell>{key}</DataGridHeadCell>
-                <DataGridCell>
-                  <Stack gap="1" direction="horizontal" wrap>
-                    {value}
-                  </Stack>
-                </DataGridCell>
-              </DataGridRow>
-            ))}
-          </DataGrid>
-        ) : (
-          <p>No labels found.</p>
-        )}
-      </Container>
+            <Container py px={false}>
+              <p className={sectionHeaderStyles}>Labels</p>
+              {cluster.labels && Object.keys(cluster.labels).length > 0 ? (
+                <DataGrid columns={2} minContentColumns={[0]}>
+                  {Object.entries(cluster.labels).map(([key, value]) => (
+                    <DataGridRow key={key}>
+                      <DataGridHeadCell>{key}</DataGridHeadCell>
+                      <DataGridCell>
+                        <Stack gap="1" direction="horizontal" wrap>
+                          {value}
+                        </Stack>
+                      </DataGridCell>
+                    </DataGridRow>
+                  ))}
+                </DataGrid>
+              ) : (
+                <p>No labels found.</p>
+              )}
+            </Container>
 
-      <Container py px={false}>
-        <p className={sectionHeaderStyles}>Rediness</p>
-        {cluster.readiness && cluster.readiness.conditions && cluster.readiness.conditions.length > 0 ? (
-          <DataGrid columns={2} minContentColumns={[0]}>
-            <DataGridRow>
-              <DataGridHeadCell>Rediness</DataGridHeadCell>
-              <DataGridCell>
-                <RedinessConditions conditions={cluster.readiness.conditions} />
-              </DataGridCell>
-            </DataGridRow>
-          </DataGrid>
-        ) : (
-          <p>No readiness conditions found.</p>
-        )}
-      </Container>
+            <Container py px={false}>
+              <p className={sectionHeaderStyles}>Rediness</p>
+              {cluster.readiness && cluster.readiness.conditions && cluster.readiness.conditions.length > 0 ? (
+                <DataGrid columns={2} minContentColumns={[0]}>
+                  <DataGridRow>
+                    <DataGridHeadCell>Rediness</DataGridHeadCell>
+                    <DataGridCell>
+                      <RedinessConditions conditions={cluster.readiness.conditions} />
+                    </DataGridCell>
+                  </DataGridRow>
+                </DataGrid>
+              ) : (
+                <p>No readiness conditions found.</p>
+              )}
+            </Container>
 
-      {/* JSON Modal */}
-      <Modal size="large" onCancel={() => setDisplayJson(false)} open={displayJson}>
-        <JsonViewer expanded={2} data={cluster} toolbar />
-      </Modal>
-    </>
+            {/* JSON Modal */}
+            <Modal size="large" onCancel={() => setDisplayJson(false)} open={displayJson}>
+              <JsonViewer expanded={2} data={cluster} toolbar />
+            </Modal>
+          </>
+        )
+      }}
+    </Await>
   )
 }
