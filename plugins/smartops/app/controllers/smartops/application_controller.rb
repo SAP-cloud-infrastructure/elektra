d0d3b68f3cc01b108889accc88d86ad2d02e0980
hgw77 <hans-georg.winkler@sap.com>
2025-09-30 14:29:11 +0200

    feat(smartops): add service layer and menu entry

--- plugins/smartops/app/controllers/smartops/application_controller.rb
+++ plugins/smartops/app/controllers/smartops/application_controller.rb
@@ -2,5 +2,8 @@
 
 module Smartops
   class ApplicationController < DashboardController
+    def show
+      services.smartops.list_jobs(status: 'initial', project_id: @scoped_project_id)
+    end
   end
 end
