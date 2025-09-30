65b74f2267d64dbc1f5547e0fb270558037152b1
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-09-30 16:11:35 +0200

    chore(kubernetes): extract basepath and mountpoint directly

--- plugins/kubernetes_ng/app/javascript/widgets/app/App.tsx
+++ plugins/kubernetes_ng/app/javascript/widgets/app/App.tsx
@@ -9,14 +9,14 @@ interface AppProps {
   mountpoint: string
 }
 
-export default function App(props: AppProps) {
-  const router = createAppRouter(props.mountpoint)
+export default function App({ basepath, mountpoint }: AppProps) {
+  const router = createAppRouter(mountpoint)
 
   return (
     <AppShellProvider theme="theme-light">
       <style>{styles}</style>
       <StrictMode>
-        <RouterProvider basepath={props.basepath} context={{}} router={router} />
+        <RouterProvider basepath={basepath} context={{}} router={router} />
       </StrictMode>
     </AppShellProvider>
   )
