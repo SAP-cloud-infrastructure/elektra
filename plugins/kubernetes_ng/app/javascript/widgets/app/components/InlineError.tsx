85c20544f661b5005f80932a7b20817d53d08659
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-10-04 16:29:15 +0200

    chore(kubernetes): added error handling for tanstak router network errors

--- plugins/kubernetes_ng/app/javascript/widgets/app/components/InlineError.tsx
+++ plugins/kubernetes_ng/app/javascript/widgets/app/components/InlineError.tsx
@@ -1,21 +1,36 @@
 import React from "react"
 import { Stack, Icon } from "@cloudoperators/juno-ui-components"
 
+function normalizeError(error: unknown): { title: string; message: string } {
+  // tanstack router errors from loaders are wrapped into the data attribute
+  // and have a __isServerError property
+  if (error && (error as any).__isServerError) {
+    return {
+      title: "Server Error: ",
+      message: (error as any).data?.message || "Please try again later.",
+    }
+  }
+  if (error instanceof Error) {
+    return { title: error.name ? `${error.name}: ` : "Error: ", message: error.message || "Something went wrong" }
+  }
+  return { title: "Unknown Error", message: "Something went wrong." }
+}
+
 interface InlineErrorProps {
-  error: Error
+  error: unknown
   className?: string
   [key: string]: any
 }
 
 const InlineError = ({ error, className, ...props }: InlineErrorProps) => {
-  const errorName = error.name ? `${error.name}: ` : "Error: "
-  const errorMessage = error.message || "Something went wrong"
+  const normalizedError = normalizeError(error)
+
   return (
     <Stack gap="2" alignment="center" className={`inline-error ${className}`} {...props}>
       <Icon color="tw-text-theme-danger" icon="danger" />
       <p>
-        {errorName}
-        {errorMessage}
+        {normalizedError.title}
+        {normalizedError.message}
       </p>
     </Stack>
   )
