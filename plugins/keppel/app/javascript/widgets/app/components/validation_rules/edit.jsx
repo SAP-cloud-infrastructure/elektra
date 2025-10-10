4df2f878e33278f5d6c674981c0602267a4a3724
VoigtS <s.voigt@sap.com>
2025-10-10 14:23:00 +0200

    feat(keppel): adjust the documentation according to the review

--- plugins/keppel/app/javascript/widgets/app/components/validation_rules/edit.jsx
+++ plugins/keppel/app/javascript/widgets/app/components/validation_rules/edit.jsx
@@ -40,6 +40,14 @@ export default class RBACPoliciesEditModal extends React.Component {
       rule_for_manifest: (account.validation || {}).rule_for_manifest || "",
     }
 
+    function getDocumentationLink(url, label) {
+      return (
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          {label}
+        </a>
+      )
+    }
+
     return (
       <Modal
         backdrop="static"
@@ -61,40 +69,49 @@ export default class RBACPoliciesEditModal extends React.Component {
           initialValues={initialValues}
         >
           <Modal.Body>
-            <Form.ElementHorizontal label="Validation rules" name="rule_for_manifest">
+            <Form.ElementHorizontal label="Rules" labelWidth={1} name="rule_for_manifest">
               <Form.Input elementType="input" type="text" name="rule_for_manifest" readOnly={!isAdmin} />
-              <p className="form-control-static tw-mt-2">
-                Validation rules for manifests allow you to restrict image pushes to the account in various ways, e.g. by permitting specific labels through{" "}
-                <a
-                  href="https://docs.docker.com/engine/reference/builder/#label"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LABEL commands
-                </a>{" "}
-                in the Dockerfile, or by other means.
-              </p>
-              <div>
-                <div>The rules are configured by using the Common Expression Language (CEL).</div>
-                <div>Examples of valid expressions include:</div>
-                <pre>
-                  <code>
-                    <div>&apos;label_1&apos; in labels </div>
-                    <div>&apos;label_1&apos; in labels && repo_name == &quot;repository_1&quot;</div>
-                    <div>&apos;label_1&apos; in labels || repo_name == &quot;repository_1&quot;</div>
-                  </code>
-                </pre>
-                <div>
-                  {" "}
-                  More information is available in the{" "}
-                  <a
-                    href="https://github.com/sapcc/keppel/blob/master/docs/api-spec.md#put-keppelv1accountsname"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Keppel documentation
-                  </a>
+              <div className="form-control-static tw-mt-2">
+                Setting a validation rule for manifests will restrict image pushes to only allow images that match the
+                provided rule. Validation rules for Keppel are written in{" "}
+                {getDocumentationLink("https://cel.dev/", "Common Expression Language (CEL)")}.<h4>Examples</h4>
+                <div className="tw-mb-1">
+                  Manifest validation rules can access <code>labels</code> in the manifest that were set using e.g in
+                  the image&apos;s Dockerfile. The example rule below will only allow images that link to their source
+                  code using the respective{" "}
+                  {getDocumentationLink(
+                    "https://github.com/opencontainers/image-spec/blob/main/annotations.md#pre-defined-annotation-keys",
+                    "well known label"
+                  )}
+                  :
                 </div>
+                <pre>
+                  <code>&quot;org.opencontainers.image.source&quot; in labels</code>
+                </pre>
+              </div>
+              <div className="tw-mb-1">
+                Manifest validation rules can access the <code>repo_name</code> where the manifest is being pushed to.
+                The example rule below requires that manifests pushed to a repo starting with <code>official/</code>{" "}
+                refer to a source repository below a certain GitHub organization:
+              </div>
+              <pre>
+                <code>
+                  <div>repo_name.startsWith(&quot;official/&quot;)</div>
+                  <div className="tw-ml-2">
+                    ?
+                    labels[&quot;org.opencontainers.image.source&quot;].startsWith(&quot;https://github.com/example-official/&quot;)
+                    <div>: true</div>
+                  </div>
+                </code>
+              </pre>
+              <div>
+                The{" "}
+                {getDocumentationLink(
+                  "https://github.com/sapcc/keppel/blob/master/docs/api-spec.md#put-keppelv1accountsname",
+                  "API reference in the Keppel documentation"
+                )}{" "}
+                contains the complete and most up-to-date list of attributes that are available inside the validation
+                rule expression.
               </div>
             </Form.ElementHorizontal>
           </Modal.Body>
