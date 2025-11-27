a0e995c0328e9ae554a947c828e3b22d1284d8f9
Andreas Pfau <andreas.pfau@sap.com>
2025-11-27 14:10:38 +0100

    feat(core): load avatar from bff (#1830)

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,31 +1,21 @@
 module AvatarHelper
   def avatar_image_tag(options = {})
     size = options.delete(:size) || 24
-    cssClass = options.delete(:class) || ""
+
+    # Use our backend to serve the avatar
+    avatar_url = "/#{@scoped_domain_fid}"
+    avatar_url += "/#{@scoped_project_fid}" if @scoped_project_fid 
+    avatar_url += "/avatar?size=#{size}"
+    fallback_url = asset_path("avatar_default.svg")
     
-    avatar_url = begin
-      # Safely interpolate the environment variable
-      url_template = ENV["MONSOON_DASHBOARD_AVATAR_URL"]
-      if url_template.present?
-        # Use gsub to replace placeholder with actual email
-        url = url_template.gsub('#{current_user.email}', current_user.email.to_s)
-                          .gsub('#{current_user.name}', current_user.name.to_s)
-        # Replace size parameter
-        url.gsub(/size=\d+x\d+/, "size=#{size}x#{size}")
-           .gsub(/s=\d+/, "s=#{size}")
-      else
-        raise "No avatar URL configured"
-      end
-    rescue StandardError => e
-      Rails.logger.debug("Avatar URL error: #{e.message}, falling back to Gravatar")
-      email_hash = Digest::MD5.hexdigest(current_user.email.to_s.downcase.strip)
-      "https://www.gravatar.com/avatar/#{email_hash}?d=mm&s=#{size}"
-    end
-    
-    tag.div(class: "avatar #{cssClass}") do
-      tag.img(
+    tag.div do
+      concat tag.span(class: "spinner-gray")
+      concat tag.img(
         src: avatar_url,
-        onerror: "this.remove()",
+        width: 0,
+        height: 0,
+        onerror: "this.src='#{fallback_url}'; this.previousElementSibling.remove(); this.width='#{size}'; this.height='#{size}';",
+        onload: "this.previousElementSibling.remove(); this.width='#{size}'; this.height='#{size}';",
         loading: "lazy",
         **options
       )
