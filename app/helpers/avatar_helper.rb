8cd5aeb3efca75b4772a15fdd9e607883c0fb8d5
Andreas Pfau <andreas.pfau@sap.com>
2025-12-01 11:03:56 +0100

    fix(core): handle errors when removing the profile loading indicator (#1833)

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,8 +1,6 @@
 module AvatarHelper
   def avatar_image_tag(options = {})
     size = options.delete(:size) || 24
-
-    # Use our backend to serve the avatar
     avatar_url = "/#{@scoped_domain_fid}"
     avatar_url += "/#{@scoped_project_fid}" if @scoped_project_fid 
     avatar_url += "/avatar?size=#{size}"
@@ -14,8 +12,8 @@ module AvatarHelper
         src: avatar_url,
         width: 0,
         height: 0,
-        onerror: "this.src='#{fallback_url}'; this.previousElementSibling.remove(); this.width='#{size}'; this.height='#{size}';",
-        onload: "this.previousElementSibling.remove(); this.width='#{size}'; this.height='#{size}';",
+        onerror: "var s=this.previousElementSibling; if(s)s.remove(); this.src='#{fallback_url}'; this.width='#{size}'; this.height='#{size}';",
+        onload: "var s=this.previousElementSibling; if(s)s.remove(); this.width='#{size}'; this.height='#{size}';",
         loading: "lazy",
         **options
       )
