1f80b0421f8e4ff972d47f99c5536b92f1b781a5
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2025-12-05 14:14:15 +0100

    chore(kubernetes): remove import logRoles

--- plugins/kubernetes_ng/app/javascript/widgets/app/components/ReadinessConditions.test.tsx
+++ plugins/kubernetes_ng/app/javascript/widgets/app/components/ReadinessConditions.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen, within, fireEvent, act, logRoles } from "@testing-library/react"
+import { render, screen, within, fireEvent, act } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import ReadinessConditions from "./ReadinessConditions"
 import {
