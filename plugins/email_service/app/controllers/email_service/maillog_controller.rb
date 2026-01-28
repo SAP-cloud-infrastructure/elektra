b2a038986725b2f49d2c5a8890658d1eab1c0cff
hgw77 <hans-georg.winkler@sap.com>
2026-01-28 16:23:07 +0100

    chore(email-service): remove first step

--- plugins/email_service/app/controllers/email_service/maillog_controller.rb
+++ plugins/email_service/app/controllers/email_service/maillog_controller.rb
@@ -2,9 +2,5 @@ module EmailService
   class MaillogController < ::EmailService::ApplicationController
     authorization_context 'email_service'
     authorization_required
-
-    def index
-
-    end
   end
 end
