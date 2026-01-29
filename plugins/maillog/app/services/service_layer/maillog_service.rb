58e7e6aa316288584b67a62421a3d871a1b917f2
hgw77 <hans-georg.winkler@sap.com>
2026-01-29 14:19:14 +0100

    feat(maillog): add ApiController and update routes for future maillog service integration

--- plugins/maillog/app/services/service_layer/maillog_service.rb
+++ plugins/maillog/app/services/service_layer/maillog_service.rb
@@ -5,5 +5,24 @@ module ServiceLayer
       elektron.service?("nebula")
     end
 
+
+    # this for POC only to use maillog with elektron
+    # but at the moment there is no proper maillog service endpoint in openstack catalog
+    # elektron can only work with existing service endpoints
+    #def search(params = {})
+    #  # use rails https request to nebula maillog search endpoint
+    #  uri = URI.parse('https://maillog.eu-de-1.cloud.sap/v1/mails/search')
+    #  request = Net::HTTP::Get.new(uri)
+    #  http = Net::HTTP.new(uri.host, uri.port)
+    #  http.use_ssl = true
+    #  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    #  response = http.request(request)
+    #  puts response.body
+    #  #elektron.service("nebula").get(
+    #  #  "/mails/search",
+    #  #  params
+    #  #)
+    #end
+
   end
 end
