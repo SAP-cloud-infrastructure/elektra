b3d939a9a420c632cf9264eb30cd20613e66b006
hgw77 <hans-georg.winkler@sap.com>
2026-01-29 14:18:56 +0100

    chore(maillog): clean up ApplicationController by removing unused methods and comments

--- plugins/maillog/app/controllers/maillog/application_controller.rb
+++ plugins/maillog/app/controllers/maillog/application_controller.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module Maillog
-  # Maillog ApplicationController
   class ApplicationController < ::DashboardController
-    authorization_context "maillog"
-    authorization_required
-
-    def index
-    end
-
   end
 end
