02930850adfb98380eb68081d07c4bbc46aafd7a
Andreas Pfau <andreas.pfau@sap.com>
2026-02-27 13:19:53 +0100

    chore(core): cleanup obsolete code

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
-# # Be sure to restart your server when you modify this file.
-# Rails.application.config.session_store :active_record_store,
-#                                        key: "_monsoon-dashboard_session"
-
 # config/initializers/session_store.rb
 Rails.application.config.session_store :cookie_store,
                                        key: "_monsoon-dashboard_session",
