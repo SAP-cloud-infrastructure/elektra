ed17b9dd9f2d311e12917f02edbd5811ed3aed38
Arturo Reuschenbach Puncernau <reuschenbach@gmail.com>
2026-03-17 16:22:37 +0100

    chore(kubernetes): fix lint

--- plugins/kubernetes_ng/app/javascript/widgets/app/routes/clusters/-components/HeadingInfo.test.tsx
+++ plugins/kubernetes_ng/app/javascript/widgets/app/routes/clusters/-components/HeadingInfo.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen, act } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
-import userEvent from "@testing-library/user-event"
 import HeadingInfo from "./HeadingInfo"
 
 describe("HeadingInfo", () => {
