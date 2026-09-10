8d420ca6ac851ba36512f315dc458aeff0cf988c
VoigtS <s.voigt@sap.com>
2026-09-10 18:05:55 +0200

    chore(deps): update limesui to 1.21.0

--- plugins/resources/app/controllers/resources/v2_controller.rb
+++ plugins/resources/app/controllers/resources/v2_controller.rb
@@ -1,7 +1,18 @@
 module Resources
   class V2Controller < DashboardController
-    def project; end 
+    helper_method :resource_documentation_links
+
+    def project; end
     def domain; end
     def cluster; end
-  end 
+
+    private
+
+    def resource_documentation_links
+      {
+        "share_replication" => "https://documentation.global.cloud.sap/docs/customer/storage/file-storage/fs-howto/filestore-create-a-share-replica/",
+        "convert_commitments" => "https://documentation.global.cloud.sap/docs/customer/monitoring/quota-management/convert-resources/"
+      }
+    end
+  end
 end
